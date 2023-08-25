72d486e2f280d926181592ee08cf5be80e9b0095
SammuelBlebo <samuel.blebo@yahoo.com>
2023-08-25 04:59:36 +0000

    .

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-
+const pool = require("./db");
 // middleware
 app.use(cors("cors"));
 app.use(express.json());
@@ -9,3 +9,13 @@ app.use(express.json());
 app.listen(5000, () => {
   console.log("server has started on port 5000");
 });
+
+//ROUTES
+
+//create a book
+
+// get all book
+
+// get a book
+
+// update a book
