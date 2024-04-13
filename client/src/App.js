6edaed6d345ca73390bd9a51a63440a0153aea5c
Samuel <samuel.blebo@yahoo.com>
2024-04-13 13:42:30 +0000

    Fix: App re-initiated  with create react app.

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
-import React, { Fragment } from "react";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
